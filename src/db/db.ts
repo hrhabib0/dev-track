69a61a99e8e679e927232c88675be7f880d49d86
hrhabib0 <hrhabib398668@gmail.com>
2026-05-22 10:22:04 +0600

    Created issues table on the database.

--- src/db/db.ts
+++ src/db/db.ts
@@ -20,6 +20,27 @@ export const initDb = async () => {
             )
             `)
 
+
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS issues(
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(150) NOT NULL,
+
+            description TEXT NOT NULL
+                CHECK(char_length(description)>=20),
+
+            type VARCHAR(20) NOT NULL
+                CHECK(type IN('bug', 'feature_request')),
+
+            status VARCHAR(20) DEFAULT 'open'
+                CHECK(status IN('open', 'in_progress', 'resolved')),
+
+            reporter_id INTEGER NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+            `)
+
         console.log("Database connect successfully")
     } catch (error) {
         console.log(error)
