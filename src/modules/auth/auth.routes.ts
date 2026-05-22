4aa88eb86cf8ec34fa0d4b8b7279a0e4a7bad1cf
hrhabib0 <hrhabib398668@gmail.com>
2026-05-22 18:57:20 +0600

    fix a bug in login feature.

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { authController } from "./auth.controller";
-import auth from "../../middleware/auth";
 
 const router = Router();
 
 router.post('/signup', authController.signUpUser);
-router.post('/login', auth(), authController.logInUser);
+router.post('/login', authController.logInUser);
 
 export const authRoute = router;
